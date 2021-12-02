baed4afbd2c3c497d4121c9c4e131ccb7a799d2f
c1040783 <c1040783@newcastle.ac.uk>
2021-12-02 18:37:37 +0000

    update reports and few modifications

--- src/leaving_reason.R
+++ src/leaving_reason.R
@@ -33,8 +33,6 @@ convert2 <- function(leaving){
 duration <- function(leaving){
   leaving$duration = difftime(leaving$left_at, leaving$enrolled_at,
                                     units = c("days"))
-  #leaving$duration <- as.numeric(leaving$duration, units="weeks")
-  #leaving$duration <- round(leaving$duration, digits=0)
   return(leaving$duration)
 }
 
@@ -44,6 +42,14 @@ leaving4$enrolled_at <- convert1(leaving4); leaving4$left_at <- convert2(leaving
 leaving5$enrolled_at <- convert1(leaving5); leaving5$left_at <- convert2(leaving5); leaving5$duration <- duration(leaving5)
 leaving6$enrolled_at <- convert1(leaving6); leaving6$left_at <- convert2(leaving6); leaving6$duration <- duration(leaving6)
 leaving7$enrolled_at <- convert1(leaving7); leaving7$left_at <- convert2(leaving7); leaving7$duration <- duration(leaving7)
+
+#how many person leaves each run
+leaving_num = c(1:4) 
+#get the percentage of students who fully participated in the course
+leaving_num[1] = dim(leaving4)[1]/dim(enrolments4)[1] * 100
+leaving_num[2] = dim(leaving5)[1]/dim(enrolments5)[1] * 100
+leaving_num[3] = dim(leaving6)[1]/dim(enrolments6)[1] * 100
+leaving_num[4] = dim(leaving7)[1]/dim(enrolments7)[1] * 100
 
 #index and bind the data to enable easier graphing 
 leaving4['run'] <- 4; leaving5['run'] <- 5; 
@@ -57,21 +63,23 @@ merged_leaving <- merged_leaving %>%
   group_by(leaving_reason) %>%
   mutate(reason_num = cur_group_id())
 
-#plot the duration before leaving against the starting date for all of the run 
-
 #create gg plot
-graph1 <- ggplot(data = merged_leaving, aes(
-  x = enrolled_at, y = duration, color = as.factor(run))) +
-  xlab("Enrolled date") + ylab("Duration before leaving (days)") + 
+graph3 <- ggplot(data = merged_leaving, aes(
+  x = left_at, y = duration, color = as.factor(run))) +
+  xlab("Leaving date") + ylab("Duration before leaving (days)") + 
   geom_point() +
   guides(color = guide_legend(title = "Run")) + 
   scale_x_date(date_breaks = "3 months" , date_labels = "%b-%y")
 
 #change color schemes
-graph1 + scale_color_manual(c("#C4961A", "#F4EDCA", "#D16103", "#C3D7A4", 
+graph3 + scale_color_manual(c("#C4961A", "#F4EDCA", "#D16103", "#C3D7A4", 
                               "#52854C", "#4E84C4", "#293352"))
 
+#plot the percentage of leaving rate against each run 
+graph4 <- qplot(4:7, leaving_num, colour = I("blue"))
+#plot the duration before leaving against the starting date for all of the run 
+
 #plot the compilation results of why students leave the course
-graph2 <- ggplot(data.frame(merged_leaving), aes(x = reason_num)) +
+graph5 <- ggplot(data.frame(merged_leaving), aes(x = reason_num)) +
   geom_bar() +
   scale_x_continuous(breaks = c(1:8))
